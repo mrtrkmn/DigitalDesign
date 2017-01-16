749ea1d6c5ed3f0b06ce9bb66ae798db70185049
Ahmet Türkmen <ahmetturkmen@users.noreply.github.com>
2017-01-16 23:19:30 +0200

    module_1

---
+++ LAB1/model_1.vhd
@@ -0,0 +1,46 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 10/08/2016 12:41:25 AM
+-- Design Name: 
+-- Module Name: model_1 - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity model_1 is
+    Port ( A : in STD_LOGIC;
+           B : in STD_LOGIC;
+           C : in STD_LOGIC;
+           D : out STD_LOGIC);
+end model_1;
+
+architecture Behavioral of model_1 is
+
+begin
+D <= A and B and C;
+
+end Behavioral;
