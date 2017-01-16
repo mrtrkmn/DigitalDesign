40d517674bee9f4c6fbe60831a6c40fe5871be1a
Ahmet Türkmen <ahmetturkmen@users.noreply.github.com>
2017-01-16 23:14:50 +0200

    module_2

---
+++ LAB1/module_2.vhd
@@ -0,0 +1,46 @@
+
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 10/08/2016 12:42:02 AM
+-- Design Name: 
+-- Module Name: module_2 - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity module_2 is
+    Port ( X : in STD_LOGIC;
+           Y : in STD_LOGIC;
+           Z : in STD_LOGIC;
+           T : out STD_LOGIC);
+end module_2;
+
+architecture Behavioral of module_2 is
+
+begin
+T<= X or Y or Z;
+end Behavioral;
