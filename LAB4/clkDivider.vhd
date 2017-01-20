4cf9b39591fd4b9c3caf4cee6095f5bbb01bae73
Ahmet Türkmen <ahmetturkmen@users.noreply.github.com>
2017-01-20 19:34:21 +0200

    Decoder part

---
+++ LAB4/clkDivider.vhd
@@ -0,0 +1,34 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+
+entity clockDivider is
+    Port ( clockIn : in STD_LOGIC;
+           resetClk : in STD_LOGIC;
+           clockOut : out STD_LOGIC);
+end clockDivider;
+
+architecture Behavioral of clockDivider is
+
+signal temp : STD_LOGIC;
+begin
+
+frequencyDivider: process (clockIn,resetClk)
+                  variable count : integer range 0 to 124999 := 0;  
+                  begin
+                    if resetClk = '1' then
+                        temp <= '0';
+                        count := 0;
+                    elsif rising_edge(clockIn) then
+                        if (count = 124999) then
+                            temp <= NOT(temp);
+                            count := 0;
+                        else
+                            count := count + 1;
+                        end if;
+                    end if;
+                    
+                    end process;
+                    
+   clockOut <= temp;
+end Behavioral;
